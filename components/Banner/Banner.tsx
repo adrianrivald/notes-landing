2b3719f80e36c0fb130ec93dca515a355e36044a
adrianrivald <adrianrivaldy@gmail.com>
2024-12-23 11:54:58 +0700

    fix

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -14,7 +14,7 @@ export function Banner({ t }: BannerProps) {
   return (
     // <div className="w-full h-[350px] bg-gradient-to-r from-[#3080E2] to-[#195EB3] mt-24 relative">
     <div className={twMerge("mt-32 p-4 lg:p-16  ", maxWidthContainer)}>
-      <div className="flex p-8 lg:p-24 rounded-xl bg-gradient-to-r from-[#3080E2] to-[#195EB3]">
+      <div className="flex p-8 lg:p-24 rounded-xl bg-gradient-to-r from-[#3080E2] to-[#195EB3] relative min-h-[400px]">
         <div className="lg:w-[60%] flex flex-col items-center lg:block">
           <h2 className="text-[24px] lg:text-[36px] text-center lg:text-left text-white mb-8 font-bold">
             {t("home.bannerHeading")}
@@ -22,6 +22,26 @@ export function Banner({ t }: BannerProps) {
           <Link href="https://memos-landing.vercel.app/demo" target="_blank">
             <Button title={t("home.tryDemo")} className="w-[220px]" />
           </Link>
+          <img
+            src="/assets/images/demo/1.png"
+            className="hidden lg:block absolute right-96 top-24"
+          />
+          <img
+            src="/assets/images/demo/2.png"
+            className="hidden lg:block absolute right-60 top-16"
+          />
+          <img
+            src="/assets/images/demo/3.png"
+            className="hidden lg:block absolute right-44 top-24"
+          />
+          <img
+            src="/assets/images/demo/4.png"
+            className="hidden lg:block absolute right-[26rem] top-56"
+          />
+          <img
+            src="/assets/images/demo/5.png"
+            className="hidden lg:block absolute right-44 top-36"
+          />
         </div>
       </div>
     </div>

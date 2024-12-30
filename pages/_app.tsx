4cb2e4e6dd6df3b4254d98128faaed0ad35c2f3d
adrianrivald <adrianrivaldy@gmail.com>
2024-12-30 13:57:20 +0700

    feat: gtm notes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,48 @@
+import React from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Header, Footer } from "../components";
 import { appWithTranslation } from "next-i18next";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { gtmVirtualPageView } from "../libs";
 
 export function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const mainDataLayer = {
+      pageTypeName: pageProps.page || null,
+      url: router.pathname,
+    };
+
+    gtmVirtualPageView(mainDataLayer);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pageProps]);
+
   return (
-    // max-w-[100rem]
-    <section className="w-full mx-auto text-neutral-500">
-      <Header />
-      <div className="mt-[5.5rem] lg:mt-0">
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    </section>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-PEGTBQVR2N"
+      ></Script>
+      <Script>
+        {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+         
+           gtag('config', 'G-PEGTBQVR2N');`}
+      </Script>
+      <section className="w-full mx-auto text-neutral-500">
+        <Header />
+        <div className="mt-[1.5rem] lg:mt-0">
+          <Component {...pageProps} />
+          <Footer />
+        </div>
+      </section>
+    </>
   );
 }
 

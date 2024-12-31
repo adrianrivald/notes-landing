df991b73e5278ca61a3693b5a914e230fcfe1de3
adrianrivald <adrianrivaldy@gmail.com>
2024-12-31 12:12:18 +0700

    feat: script gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,6 +35,22 @@ export function App({ Component, pageProps }: AppProps) {
          
            gtag('config', 'G-PEGTBQVR2N');`}
       </Script>
+
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5QSPQBH3');
+        `}
+      </Script>
+
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QSPQBH3" height="0" width="0" style="display: none; visibility: hidden;" />`,
+        }}
+      />
       <section className="w-full mx-auto text-neutral-500">
         <Header />
         <div className="mt-[1.5rem] lg:mt-0">
